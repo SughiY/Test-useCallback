8f604ed51b5dbdc6344b89b34c255fff76f95866
Shiyi Gu <shiyi.gu@airbnb.com>
2019-12-09 20:57:24 +0100

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useCallback } from "react";
+
+const Component = (props) => (<div>{props.children}</div>);
+const MemoComponent = React.memo(Component);
+
+const Text = ({text}) => (<div>{text}</div>);
+
+function Weather({ title, date, handler }) {
+  return (
+    <div>
+      <div>{handler}</div>
+      <Text text={`Weather: ${title}`} />
+      <Text text={`date: ${date}`} />
+    </div>
+  );
+}
+export const MemoizedWeather = React.memo(Weather);
+
+const MemoWrapper = React.memo(({ title, date }) => (
+  <Component>
+    <Weather title={title} date={date} handler={() => {}} />
+  </Component>
+));
+
+const Button = (props) => (<button onClick={props.onClick}>{props.children}</button>)
+
+export default (() => {
+
+  const [weather, setWeather] = useState({ title: "Sun", date: "Yesterday" });
+
+  const memo_func = useCallback(()=> {}, []);
+
+  return (
+    <div>
+      <br/>
+      <div> Component with arrow function </div>
+      <Weather {...weather} handler={() => {}} />
+      <br/>
+      <div> Memocomponent with arrow function </div>
+      <MemoizedWeather {...weather} handler={() => {}}/>
+      <br/>
+      <div> Component with useCallback function </div>
+      <Weather title={"test"} date={"Today"} handler={memo_func} />
+      <br/>
+      <div> MemoComponent with useCallback function </div>
+      <MemoizedWeather {...weather} handler={memo_func}/>
+      <br/>
+      <div> MemoParent with arrow function on child </div>
+      <MemoWrapper {...weather} />
+      <br/>
+      <div><Button onClick={() => setWeather({ title: "Sun", date: "Yesterday" })}>Yesterday is a good day</Button></div>
+      <div><Button onClick={() => setWeather({ title: "Snow", date: "Tomorrow" })}>Tommorrow has snow!</Button></div>
+      <br/>
+      <br/>
+    </div>
+  );
+
+});
